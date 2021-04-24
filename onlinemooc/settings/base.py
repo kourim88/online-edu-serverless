d2bed7a75ddf1cdb517aaf8feda86b6a0010da40
kourim88 <meiguanglin@gmail.com>
2021-04-24 16:56:14 +0900

    .envの追加。

--- onlinemooc/settings/base.py
+++ onlinemooc/settings/base.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
