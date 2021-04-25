23ae7ca53025985f18e40711801f1c20e9ee4421
kourim88 <meiguanglin@gmail.com>
2021-04-26 00:08:40 +0900

    行の削除。

--- onlinemooc/settings/base.py
+++ onlinemooc/settings/base.py
@@ -16,8 +16,6 @@ import environ
 env = environ.Env()
 env.read_env('conf/.env')
 
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
